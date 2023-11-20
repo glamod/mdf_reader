cab232c16c17e0407927c49ce0dccf9498c7b4b8
ludwig.lierhammer <ludwig.lierhammer@dwd.de>
2023-11-20 10:10:30 +0000

    read version from _version.py

--- mdf_reader/__init__.py
+++ mdf_reader/__init__.py
@@ -1,6 +1,16 @@
 # Following to access the subpackages main modules (or/and functions) directly wihout loops through the full subpackage path
 from .data_models import code_tables  # noqa
 from .data_models import schemas  # noqa
-from mdf_reader.read import main as read  # noqa
+from mdf_reader.read import read  # noqa
 
-__version__ = "1.3"
+
+def _get_version():
+    __version__ = "unknown"
+    try:
+        from ._version import __version__
+    except ImportError:
+        pass
+    return __version__
+
+
+__version__ = _get_version()
